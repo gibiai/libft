26018ed79a7bb1565e203a7f907f9cb563636333
Gabriele De Carlo <gibiai123@gmail.com>
2023-04-11 20:13:51 +0200

    Add files via upload

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,27 +6,27 @@
 /*   By: gde-carl <gde-carl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 01:58:24 by gde-carl          #+#    #+#             */
-/*   Updated: 2023/04/07 19:44:03 by gde-carl         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:40:18 by gde-carl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	char	*str;
-	int		i;
+{	
+	unsigned int	i;
+	char			*str;
 
-	if (!s)
+	i = 0;
+	if (!s || !f)
 		return (NULL);
-	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
 	if (!str)
 		return (NULL);
-	i = 0;
 	while (s[i])
-	{
+	{	
 		str[i] = f(i, s[i]);
-		i++;
+		i ++;
 	}
 	str[i] = '\0';
 	return (str);
